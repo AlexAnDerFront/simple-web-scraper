7378e75593ab8843ed277ecae60625bef01a0e8b
Alexanderfront <alexistamarbeiten@gmail.com>
2025-07-10 17:49:43 +0200

    feat: added page interaction function

--- scraper.js
+++ scraper.js
@@ -15,25 +15,6 @@ async function loadPage() {
 /*     console.log(pageContents);
     console.log('pageTitle= ', pageTitle);
     console.log('it worked'); */
-
-    const filesToDelete = [
-        path.join(__dirname, 'codes', 'web.pdf'),
-        path.join(__dirname, 'codes', 'screenshot1.png'),
-        path.join(__dirname, 'codes', 'screenshot2.png')
-    ];
-
-    for (const filePath of filesToDelete) { // delete old files function
-      try {
-        await fs.unlink(filePath);
-        console.log(`Deleted: ${filePath}`);
-      } catch (err) {
-        if (err.code === 'ENOENT') {
-          console.log(`File not found (skipped): ${filePath}`);
-        } else {
-          console.error(`Error deleting ${filePath}:`, err);
-        }
-      }
-    }
     
     await page.screenshot({path: "screenshot1.png"});   // get page screenshot
     await page.pdf({path: "web.pdf", format: "a4"});   // get page pdf
@@ -50,10 +31,23 @@ async function loadPage() {
 
     let linkselect ='li.ast-grid-common-col:nth-child(1) > div:nth-child(2) > a:nth-child(1)'
 
-    page.click(linkselect);
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    page.click(linkselect); 
+    await new Promise(resolve => setTimeout(resolve, 2000));
     await page.waitForSelector('.variations_form', { visible: true });
     await page.screenshot({path: "screenshot2.png"}); 
+
+    let searchTerm = "shirt";
+    page.evaluate((searchTerm)=> {
+      let input = document.querySelector('#wp-block-search__input-1')
+      let form = document.querySelector('aside.header-widget-area:nth-child(1) > section:nth-child(1) > form:nth-child(1)')
+
+      input.value = searchTerm
+
+      form.submit();
+      
+    }, searchTerm);
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await page.screenshot({path: "screenshot3.png"}); 
     await browser.close();
 }
 loadPage();
